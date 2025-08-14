6cb6f97033d782645cc57cd1a7d7d20c4286c77b
Jay <jp6631@srmist.edu.in>
2025-08-14 21:53:08 +0530

    Analyzers functions

--- Frontend/analyzers.py
+++ Frontend/analyzers.py
@@ -10,6 +10,7 @@ gender_model = load_model("saved_models/gender_model.keras")
 gender_labels = ["Male","Female"]
 
 def predict_age_gender(image):
+    """Predicts age and gender from a face image."""
     img_resized = cv2.resize(image,(224,224))/255.0
     img_expanded = np.expand_dims(img_resized,axis=0)
 
@@ -19,3 +20,17 @@ def predict_age_gender(image):
 
     return int(age_pred),gender_pred
 
+# Fatigue Estimation
+
+fatigue_model = load_model("saved_models/best_fatigue_model.keras")
+
+
+def predict_fatigue(image):
+    """Predicts fatigue based on facial/eye features."""
+    img_resized = cv2.resize(image, (224, 224)) / 255.0
+    img_expanded = np.expand_dims(img_resized, axis=0)
+
+    pred = fatigue_model.predict(img_expanded)
+    return "Fatigued" if pred[0][0] > 0.5 else "Not Fatigued"
+
+# Extract face image
