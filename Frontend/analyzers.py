2a29fcdf752990678aa369377fb46e0997c61e43
Jay <jp6631@srmist.edu.in>
2025-08-14 21:41:56 +0530

    Analyzers functions

---
+++ Frontend/analyzers.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+from tensorflow.keras.models import load_model
+
+
+# Age and Gender Prediction
+
+age_model = load_model("saved_models/age_model.keras")
+gender_model = load_model("saved_models/gender_model.keras")
+gender_labels = ["Male","Female"]
+
+def predict_age_gender(image):
+    img_resized = cv2.resize(image,(224,224))/255.0
+    img_expanded = np.expand_dims(img_resized,axis=0)
+
+    age_pred = age_model.predict(img_expanded)[0][0]
+    gender_pred = gender_model.predict(img_expanded)
+    gender_label = gender_labels[np.argmax(gender_pred)]
+
+    return int(age_pred),gender_pred
+
